d6472754391cf932a1112c5666d87bbe0a212ad8
SaadMeer <saad.wicks@gmail.com>
2023-11-02 21:39:56 +0500

    Login & signin page

--- app.js
+++ app.js
@@ -13,8 +13,31 @@ function toggleMenu() {
 
 function closeMenu() {
     var menuContainer = document.querySelector('.menu-conatiner');
-  
+
     if (menuContainer) {
-      menuContainer.style.width = '0px';
+        menuContainer.style.width = '0px';
     }
-  }
+}
+
+
+const toggleBox = document.querySelector('.toggle-box-container');
+
+toggleBox.addEventListener('click', () => {
+    const sidebarMain = document.querySelector('.sidebar-main-container');
+    const sidebarSpan = document.querySelector('.sidebar-ul');
+    const sidebarli = document.querySelectorAll('.sidebar-li');
+    const spans = sidebarSpan.getElementsByTagName("span");
+
+    document.querySelector('.right-content-section').classList.toggle('right-content-section-flex')
+    sidebarMain.classList.toggle("hide-sidebar");
+    toggleBox.classList.toggle("toggle-box-container-show");
+
+    for (let i = 0; i < spans.length; i++) {
+        spans[i].classList.toggle("dislpay-none");
+
+    }
+    for (let i = 0; i < sidebarli.length; i++) {
+        sidebarli[i].classList.toggle("sidebar-icon-center");
+    }
+
+})
